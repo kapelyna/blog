3f71db52482916693bbc09eb16aebef9c3307e80
kapelyna <katiakaplya@gmail.com>
2024-10-05 16:25:31 +0300

    blog version 1.1

--- app/mailers/comments_mailer.rb
+++ app/mailers/comments_mailer.rb
@@ -1,13 +1,6 @@
 class CommentsMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.comments_mailer.submitted.subject
-  #
   def submitted(comment)
     @comment = comment
-    @greeting = "Hi"
-
-    mail to: "blog-owner@example.org", subject: "New comment!"
+    mail(to: "blog-owner@example.org", subject: "New Comment Submitted")
   end
 end
--- test/mailers/previews/comments_mailer_preview.rb
+++ test/mailers/previews/comments_mailer_preview.rb
@@ -1,7 +1,8 @@
-# Preview all emails at http://localhost:3000/rails/mailers/comments_mailer
 class CommentsMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/comments_mailer/submitted
   def submitted
-    CommentsMailer.submitted Comment.first
+    post = Post.first || Post.create(title: "Sample Post", content: "Sample content") # Ensure there's a post
+    comment = Comment.create(content: "This is a sample comment", post: post)
+    CommentsMailer.submitted(comment)
   end
 end
